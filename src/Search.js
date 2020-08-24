b7ae8f482e4b154c759495ad1681a556118b6ca0
Michael Singhurse <michaelsinghurse@gmail.com>
2020-08-24 17:31:44 -0500

    changed input attribute for to htmlFor

--- src/Search.js
+++ src/Search.js
@@ -9,21 +9,21 @@ export default function Search() {
           <legend>Enter Your Sales Location</legend>
           <ul> 
             <li>
-              <label for="street" className="label-street">Street:</label>
+              <label htmlFor="street" className="label-street">Street:</label>
               <input id="street" type="text" className="input-street" required/>
             </li>
             <li>
-              <label for="city">City:</label>
+              <label htmlFor="city">City:</label>
               <input id="city" type="text" className="input-city" required/>
             </li>
             <li>
-              <label for="state">State:</label>
+              <label htmlFor="state">State:</label>
               <input id="state" type="text" className="input-state"
                 pattern="[A-Za-z]{2}" 
                 title="Two letter state abbreviation" required/>
             </li>
             <li>
-              <label for="zip">Zip:</label>
+              <label htmlFor="zip">Zip:</label>
               <input id="zip" type="text" className="input-zip" 
                 pattern="\d{5}"
                 title="Five digit zip code" required/>
