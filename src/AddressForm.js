88afc3e23ea14811468428ca49be67708cd18bb2
Michael Singhurse <michaelsinghurse@gmail.com>
2020-09-03 17:31:41 -0500

    handled form submit, added state. still need to validate inputs

--- src/AddressForm.js
+++ src/AddressForm.js
@@ -4,46 +4,82 @@ export default class AddressForm extends React.Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      street: "",
+      city: "",
+      state: "",
+      zip: "",
+    };
+
+    this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.id]: event.target.value,
+    });
   }
 
   handleSubmit(event) {
     event.preventDefault();
-    console.log("Handling submit within AddressForm class");
-    this.props.onFormSubmit();
+
+    this.props.onFormSubmit({
+      street: this.state.street,
+      city: this.state.city,
+      state: this.state.state,
+      zip: this.state.zip,
+    });
+
+    this.setState({
+      street: "",
+      city: "",
+      state: "",
+      zip: "",
+    });
   }
+  
+  // TODO: form validation
+  // - all inputs are required
+  // - state must be two letters
+  // - zip must be five digits
+  // - use novalidate attribute ("noValidate") on form and input elements?
 
   render() {
     return (
-      <form action="#" method="get" className="search-form" 
-        onSubmit={this.handleSubmit}>
-        <fieldset>
-          <legend>Enter Your Sales Location</legend>
-          <ul> 
-            <li>
-              <label htmlFor="street" className="label-street">Street:</label>
-              <input id="street" type="text" className="input-street" required/>
-            </li>
-            <li>
-              <label htmlFor="city">City:</label>
-              <input id="city" type="text" className="input-city" required/>
-            </li>
-            <li>
-              <label htmlFor="state">State:</label>
-              <input id="state" type="text" className="input-state"
-                pattern="[A-Za-z]{2}" 
-                title="Two letter state abbreviation" required/>
-            </li>
-            <li>
-              <label htmlFor="zip">Zip:</label>
-              <input id="zip" type="text" className="input-zip" 
-                pattern="\d{5}"
-                title="Five digit zip code" required/>
-            </li>
-          </ul> 
-        </fieldset>
-        <button type="submit">Find Rates</button>
-      </form>
+      <div>
+        <form 
+          className="search-form" 
+          onSubmit={this.handleSubmit} >
+          <fieldset>
+            <legend>Enter Your Sales Location</legend>
+            <ul> 
+              <li>
+                <label htmlFor="street" className="label-street">Street:</label>
+                <input id="street" type="text" className="input-street" 
+                  value={this.state.street} onChange={this.handleChange} />
+              </li>
+              <li>
+                <label htmlFor="city">City:</label>
+                <input id="city" type="text" className="input-city" 
+                  value={this.state.city} onChange={this.handleChange} />
+              </li>
+              <li>
+                <label htmlFor="state">State:</label>
+                <input id="state" type="text" className="input-state"
+                  value={this.state.state} onChange={this.handleChange} />
+              </li>
+              <li>
+                <label htmlFor="zip">Zip:</label>
+                <input id="zip" type="text" className="input-zip" 
+                  value={this.state.zip} onChange={this.handleChange} />
+              </li>
+            </ul> 
+          </fieldset>
+          <button type="submit">Find Rates</button>
+        </form>
+        <p>Form error message</p>
+      </div>
     );
   }
 }
