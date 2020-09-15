3ef42581e93915e643c756d156ec78cfeda6c84d
Michael Singhurse <michaelsinghurse@gmail.com>
2020-09-15 12:49:32 -0500

    added fetch to api

--- client/src/Search.js
+++ client/src/Search.js
@@ -3,6 +3,18 @@ import "./Search.css";
 import SearchForm from "./SearchForm";
 import SearchResults from "./SearchResults";
 
+const makeQueryString = object => {
+  const keysAndValues = Object.keys(object).reduce((array, key) => {
+    const keyAndValue = encodeURIComponent(key)
+      + "="
+      + encodeURIComponent(object[key]);
+    array.push(keyAndValue);
+    return array;
+  }, []);
+
+  return keysAndValues.join("&");
+};
+
 export default class Search extends React.Component {
   constructor(props) {
     super(props);
@@ -16,39 +28,22 @@ export default class Search extends React.Component {
   }
   
   handleFormSubmit(address) {
-    // send a get request to the server with the address
-    // append the both the address and response to this.state.results
-    
     address.searchId = this.state.searchId;
     
-    const location = {
-      city: "HOBART",
-      county: "BROWN",
-      state: "WI",
-      latLng: JSON.stringify({ lat: 39.409999, lng: -87.4022146 }),
-    };
+    const url = "/api/rates?" + makeQueryString(address);
 
-    const rates = {
-      totalRate: "0.055",
-      stateRate: "0.05",
-      countyRate: "0.005",
-      cityRate: "0",
-      totalDistrictRate: "0",
-    };
-
-    const searchResults = {
-      inputs: address,
-      location,
-      rates,
-    };
-    
-    this.setState((state, _props) => {
-      const results = [searchResults, ...state.results]; // most recent first
-      return {
-        results,
-        searchId: state.searchId + 1,
-      };
-    });
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        this.setState((state, _props) => {
+          const results = [data, ...state.results]; // most recent first
+          return {
+            results,
+            searchId: state.searchId + 1,
+          };
+        });
+      })
+      .catch(error => console.log("Error:", error));
   }
 
   render() {
