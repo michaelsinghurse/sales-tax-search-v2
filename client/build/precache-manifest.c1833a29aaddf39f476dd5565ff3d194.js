fd61f714c8fa0262e1f9dce676e39146fd470962
Michael Singhurse <michaelsinghurse@gmail.com>
2020-09-18 12:03:57 -0500

    adding build folder to source control to prepare for deployment

---
+++ client/build/precache-manifest.c1833a29aaddf39f476dd5565ff3d194.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6cbac4d506d443676642d1924c8a72d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b968e9b62de59ff280e",
+    "url": "/static/css/main.b757cf2e.chunk.css"
+  },
+  {
+    "revision": "66941787031948d30820",
+    "url": "/static/js/2.ed421722.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ed421722.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b968e9b62de59ff280e",
+    "url": "/static/js/main.606bf057.chunk.js"
+  },
+  {
+    "revision": "d23be15808ce15e5e3de",
+    "url": "/static/js/runtime-main.218b5c8c.js"
+  },
+  {
+    "revision": "a53ac8738223737ba8543426fd775376",
+    "url": "/static/media/USA_Counties.a53ac873.svg"
+  },
+  {
+    "revision": "02614f01d2fe600a9de0287b2fefca71",
+    "url": "/static/media/map-error.02614f01.png"
+  },
+  {
+    "revision": "55f6a911fd879d25bae2fff855d6ef97",
+    "url": "/static/media/michael-singhurse.55f6a911.png"
+  }
+]);
